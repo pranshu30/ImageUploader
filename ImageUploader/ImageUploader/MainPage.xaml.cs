8624a38f133601c860cf25ea8166ffcc7032a5aa
pranshu30 <pranshu.varshney30@gmail.com>
2019-01-23 13:12:09 -0800

    Full file upload

---
+++ ImageUploader/ImageUploader/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace ImageUploader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SelectImageButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "This is not supported on your device", "ok");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if(selectedImageFile == null)
+            {
+                await DisplayAlert("Error", "There was an error in uploading the file", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+
+        
+    }
+}
